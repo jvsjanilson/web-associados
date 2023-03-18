6706d6044b6849900209f0a47076d86c652c69ec
janilsonjvs <janilsonjvs@gmail.com>
2023-03-18 16:22:21 -0300

    feat: Model e Migration Profile

--- database/migrations/2014_10_11_000000_create_profiles_table.php
+++ database/migrations/2014_10_11_000000_create_profiles_table.php
@@ -13,18 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->boolean('manager')->default(0);
             $table->timestamps();
-
-            $table->unsignedBigInteger('manager_id');
-            $table->foreign('manager_id')->references('id')->on('profiles');
-            
         });
     }
 
@@ -35,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profiles');
     }
 };
